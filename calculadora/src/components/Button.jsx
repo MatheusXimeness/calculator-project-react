56267fc6c28457c3cfb6e3b77d4681b7fb59c1b9
Matheus Ximenes <matheus.ximenes2016@outlook.com>
2020-03-02 15:46:38 -0300

    fixing

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import './Button.css'
 
-
+// Quando a propriedade for passada, caso seja com alguma das opções abaixo serão aplicados os estilos
 export default props =>
-<button className='button'>{props.label}</button>
+<button className={`
+    button
+    ${props.operation ? 'operation' : ''}
+    ${props.double ? 'double' : ''}
+    ${props.triple ? 'triple' : ''}
+`}>
+    {props.label}
+</button>
