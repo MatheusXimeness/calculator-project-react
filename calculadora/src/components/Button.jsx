7232dee615c6073434ede0faaa3bb21118986c6d
Matheus Ximenes <matheus.ximenes2016@outlook.com>
2020-03-03 13:02:43 -0300

    fixing

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -3,7 +3,10 @@ import './Button.css'
 
 // Quando a propriedade for passada, caso seja com alguma das opções abaixo serão aplicados os estilos
 export default props =>
-<button className={`
+<button
+// dispara uma arrow function que recebe o evento
+    onClick = {e => props.click && props.click(props.label)}
+    className={`
     button
     ${props.operation ? 'operation' : ''}
     ${props.double ? 'double' : ''}
