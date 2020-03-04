247409bc8faddd4b3ede8857b458771dc7f8d0cf
Matheus Ximenes <matheus.ximenes2016@outlook.com>
2020-03-04 17:14:49 -0300

    ready

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -14,12 +14,12 @@ const initialState = {
 
 export default class Calculator extends Component {
 
-    // criou-se um clone do objeti 'initialState' e atribuiu a vaariavel state
+    // criou-se um clone do objeto 'initialState' e atribuiu a variavel state
     state = { ...initialState}
 
     constructor(props) {
         super(props)
-        this.clearMemory = this.addDigit.bind(this)
+        this.clearMemory = this.clearMemory.bind(this)  // define que o escopo das funções em questão é todo o Componente
         this.setOperation = this.setOperation.bind(this)
         this.addDigit = this.addDigit.bind(this)
     }
@@ -29,7 +29,29 @@ export default class Calculator extends Component {
     }
 
     setOperation(operation){
-        console.log('operation')
+        if (this.state.current === 0 ) {
+             this.setState({operation, current: 1, clearDisplay: true})
+        } else {
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+            
+            try{
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+            } catch (e) {
+                values[0] = this.state.values[0]
+            }
+
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0: 1,
+                clearDisplay: !equals,
+            })
+        }
     }
 
     addDigit(n){
